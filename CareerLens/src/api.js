d129d3fcf3fa723d4e9cf43505b83898a92f4d69
Swagath <thanabuddiswagath@gmail.com>
2026-03-12 22:45:47 +0530

    Connect frontend to Render backend

--- CareerLens/src/api.js
+++ CareerLens/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000";
+const API_BASE = "https://careerlens-nelo.onrender.com";
 
 export const analyzeResume = async (file) => {
   const formData = new FormData();
